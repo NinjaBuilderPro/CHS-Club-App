4e62d6f281de73e8379db41af06623c87a059574
NinjaBuilderPro <NinjaBuilderPro@users.noreply.github.com>
2025-01-22 16:07:59 -0500

    Fixed some UI bugs and changed ui design

--- src/components/ClubComponent.tsx
+++ src/components/ClubComponent.tsx
@@ -4,31 +4,36 @@ interface ClubProps{
     meetingTime: string
     calendar: string
 }
+var hasCalendar = true;
 const ClubComponent = ({title: title, subjects, meetingTime, calendar}: ClubProps) => {
     return ( 
         <div className="ClubComponent" style={{
             position: "relative",
             width: "84vi",
-            height: "32vi",
             top: "3vi",
+            height: `calc(22.8vi + ${hasCalendar ? "0vi" : "4.5vi"})`
         }}>
             <div style={{
                 fontSize: "4vi",
-                border: "2vi solid green",
+                border: "0.4vi solid rgb(0, 113, 4)",
                 borderRadius: "1vi",
                 position: "absolute",
                 minWidth: "30vi",
-                backgroundColor: "khaki"
+                backgroundColor: "ivory",
+                height: "6vi",
+                fontFamily: "Arial"
             }}>
                 {title}
             </div>
             <div className="ClubInfo" style={{
                 fontSize: "2vi",
-                border: "2vi solid green",
+                border: "0.4vi solid rgb(0, 113, 4)",
+                borderRadius: "1vi",
                 position: "absolute",
                 width: "80vi",
-                top: "8vi",
-                backgroundColor: "khaki"
+                top: "7vi",
+                backgroundColor: "ivory",
+                fontFamily: "Arial"
             }}>
                 <p style={{
                     fontSize: "3vi"
@@ -36,6 +41,7 @@ const ClubComponent = ({title: title, subjects, meetingTime, calendar}: ClubProp
                     Subjects: {subjects}<br/>
                     Meeting Times: {meetingTime}<br/>
                     {calendar=="" ? null : <a href={calendar}>Calendar</a>}
+                    {calendar=="" ? hasCalendar = true : hasCalendar = false}
                 </p>
             </div>
         </div>
